7208d5531fad6eceae5a0bdcc6ada9e7b48d251e
Andrew <andrewxu2003@gmail.com>
2025-08-06 15:35:39 -0700

    Adding a test file

---
+++ listTest.c
@@ -0,0 +1,14 @@
+#include "List.h"
+
+
+// This is to test the functions defined in List.c
+
+int main(){
+
+// Testing the constructor:
+  List testList = newList();
+  
+  // Test case for the initialization conditions:
+
+  return 0;
+}
