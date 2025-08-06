5a3387b797d819b0e52bc7d4464573bb896eb9cd
Andrew <andrewxu2003@gmail.com>
2025-08-06 15:48:15 -0700

    Writing first test case

--- listTest.c
+++ listTest.c
@@ -1,14 +1,16 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "List.h"
-
-
 // This is to test the functions defined in List.c
 
 int main(){
 
 // Testing the constructor:
   List testList = newList();
-  
+       
   // Test case for the initialization conditions:
-
+  
+  assert()
   return 0;
 }
