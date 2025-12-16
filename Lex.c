6ac1128fcf0ced012867edfa9e7cf6b018328053
Andrew Xu <badsheep1@Andrews-MacBook-Pro.local>
2025-12-15 18:19:42 -0800

    Writing the initial logic for Lex, checks for arguements, opens the files, and creating the framework for extracting strings from the file using fgets.

--- Lex.c
+++ Lex.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "List.h"
+
+#define MAX_LENGTH 1000
 
 int main(int argc, char *argv[]){
   
   if(argc != 3){
-    fprintf(stderr, "Usage %s <arg1> <arg2> \n", argv[0]);
-    return 1;
+    fprintf(stderr, "Error: arguement mismatch.\n"
+            "Usage: %s <input-file> <output-file> ", argv[0]);
+    exit(EXIT_FAILURE);
   }
+
+  FILE* inputFile = fopen(argv[1], "r");
+  FILE* outputFile = fopen(argv[2], "w");
+
+  int n = 0;
+  char * stringBuffer = malloc(MAX_LENGTH * sizeof(char));
+
+  while(fgets(stringBuffer, MAX_LENGTH, inputFile) != NULL){
+    
+  }
+
 
 
 
