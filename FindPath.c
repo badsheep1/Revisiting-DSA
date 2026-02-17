faebbb1f8949d8f4649cb56e87f37039e8ef9925
Andrew Xu <badsheep1@Andrews-MacBook-Pro.local>
2026-02-16 21:23:35 -0800

    Creating parsing logic for FindPath.c

--- FindPath.c
+++ FindPath.c
@@ -1,7 +1,9 @@
 #include "Graph.h" 
 #include <stdio.h>
 
-
+#define HEADER_LEN 1
+#define TOKEN_LEN 2
+#define EOF -1
 
 int main(int argc, char *argv[]){
 
@@ -13,6 +15,32 @@ int main(int argc, char *argv[]){
 
   FILE* inputFile = fopen(argv[1], "r");
   FILE* outputFile = fopen(argv[2], "w");
+
+  char inputBuffer[HEADER_LEN];
+
+  fgets(inputBuffer, TOKEN_LEN, inputFile);
+
+  int graphOrder;
+  sscanf(inputBuffer, "%d", &graphOrder); // Reads the first line of the input, saves the value as the Order of the graph.
+ 
+
+  int vertex1, vertex2;
+
+  while(fgets(inputBuffer, HEADER_LEN, inputFile)){
+    sscanf(inputBuffer, "%d, %d", &vertex1, &vertex2);
+
+    if(vertex1 && vertex2 == 0){
+      break; // Input File Terminates
+    } 
+    else{
+      
+    }
+
+
+
+  }
+  
+
 
 
   
