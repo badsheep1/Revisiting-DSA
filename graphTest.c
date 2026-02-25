2251fda5886d4f95517bd2f291947f3a600e16b6
Andrew Xu <badsheep1@Andrews-MacBook-Pro.local>
2026-02-25 11:40:52 -0800

    Tested addEdge

--- graphTest.c
+++ graphTest.c
@@ -8,10 +8,10 @@ int main(void){
 
   Graph testGraph = newGraph(5);
 
-  addArc(testGraph, 1, 2);
-  addArc(testGraph, 2, 4);
-  addArc(testGraph, 1, 3);
-  addArc(testGraph, 5, 4);
+  addEdge(testGraph, 1, 2);
+  addEdge(testGraph, 2, 4);
+  addEdge(testGraph, 1, 3);
+  addEdge(testGraph, 5, 4);
 
   FILE* outputFile = fopen("printTest.txt", "w");
 
