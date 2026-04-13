5e4b4d1c0aaa2930775fad1f3f55248da22b8c77
Andrew Xu <badsheep1@Andrews-MacBook-Pro.local>
2026-04-13 10:11:40 -0700

    Tested changeEntry

--- matrixTest.c
+++ matrixTest.c
@@ -9,6 +9,34 @@ int main(void) {
   assert(size(testMatrix) == 3);
   assert(NZZ(testMatrix) == 0);
 
+  changeEntry(testMatrix, 1, 1, 2.5);
+
+  changeEntry(testMatrix, 1, 3, 5.1);
+
+  changeEntry(testMatrix, 2, 3, 63.92);
+
+  printMatrix(stdout, testMatrix);
+
+  changeEntry(testMatrix, 2, 1, 3);
+
+  printMatrix(stdout, testMatrix);
+
+  assert(NZZ(testMatrix) == 4);
+
+  changeEntry(testMatrix, 1, 1, 3.326);
+
+  assert(NZZ(testMatrix) == 4);
+
+  printMatrix(stdout, testMatrix);
+
+  changeEntry(testMatrix, 1, 2, 0);
+
+  assert(NZZ(testMatrix) == 4);
+
+  changeEntry(testMatrix, 1, 3, 0);
+
+  assert(NZZ(testMatrix) == 3);
+
   freeMatrix(&testMatrix);
 
   return 0;
